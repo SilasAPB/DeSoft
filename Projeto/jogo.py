c66f1e46ca57d93cdc812016b504c2905d973e59
Silas A. Pereira <silasapb@al.insper.edu.br>
2023-11-01 12:05:54 -0300

    formatação fim

--- Projeto/jogo.py
+++ Projeto/jogo.py
@@ -52,22 +52,34 @@ letras=a+2
 listaescolhida=filtra(PALAVRAS,letras)
 
 dicio_inicio=inicializa(listaescolhida)
+resposta=dicio_inicio['sorteada']
 
 tentativa=0
 while tentativa<dicio_inicio['tentativas']:
     chute=input(f'Tente uma palavra com {letras} letras  ')
-    comparar=inidica_posicao(dicio_inicio['sorteada'],chute)
-    if comparar == []:
-        print(f'essa palavra não possui {letras} letras, tente novamente com uma palavra válida ')
-        tentativa=tentativa
+    if chute not in listaescolhida:
+        print('Palavra não conhecida, tente outra')
     else:
-        parcial=''
-        for pos in range(len(comparar)):
-            if comparar[pos]==0:
-                parcial+=(f'\033[0;32;40m {chute[pos]} \033[m')
-            elif comparar[pos]==1:
-                parcial+=(f'\033[0;33;40m {chute[pos]} \033[m')
-            elif comparar[pos]==2:
-                parcial+=(f'\033[0;37;40m {chute[pos]} \033[m')
-        print(parcial)
-        tentativa+=1
+        comparar=inidica_posicao(dicio_inicio['sorteada'],chute)
+        if comparar == []:
+            print(f'essa palavra não possui {letras} letras, tente novamente com uma palavra válida ')
+            tentativa=tentativa
+        else:
+            parcial=''
+            for pos in range(len(comparar)):
+                if comparar[pos]==0:
+                    parcial+=(f'\033[0;32;40m {chute[pos]} \033[m')
+                elif comparar[pos]==1:
+                    parcial+=(f'\033[0;33;40m {chute[pos]} \033[m')
+                elif comparar[pos]==2:
+                    parcial+=(f'\033[0;37;40m {chute[pos]} \033[m')
+            print(parcial)
+            a=comparar.count(0)
+            if a==letras:
+                tentativa=dicio_inicio['tentativas']
+            else:
+                tentativa+=1
+        if a==letras and tentativa<=dicio_inicio['tentativas']:
+            print('\033[1;30;43m PARABÉNS, VOCÊ DESCOBRIU A PALAVRA \033[m')
+        elif a!=letras and tentativa==dicio_inicio['tentativas']:
+            print(f'\033[1;31;40m que pena, você não descobriu... A resposta era {resposta} \033[m')
