38c6b31bb9778a2a0850b5320fd3093000bb670b
YamanduGermano <yamandu.germano@gmail.com>
2023-11-08 10:10:32 -0300

    Jogo funcional

--- Projeto/jogo.py
+++ Projeto/jogo.py
@@ -8,7 +8,9 @@ cores = {
     'correta' : '\033[0;32;40m',
     'parcial' : '\033[0;33;40m',
     'neutra' : '\033[0;37;40m',
-    'padrao' : '\033[m'
+    'padrao' : '\033[m',
+    'correta' : '\033[1;30;43m',
+    'errada' : '\033[1;31;40m'
 }
 
 # FUNÇÕES
@@ -31,9 +33,9 @@ def inicializa(lis):  # Define as configuraões do jogo
     return dicio
 
 def inidica_posicao(sort, espe):  # ("Palavra sorteada pelo sistema", "Palavra especulada pelo usuário")
-    # 
+
     lis=[]
-    if len(sort)==len(espe):
+    if len(sort)!=len(espe):
         return []
     else:
         for i in range(len(sort)):
@@ -46,36 +48,34 @@ def inidica_posicao(sort, espe):  # ("Palavra sorteada pelo sistema", "Palavra e
     return lis
 
 
-def clTxt(cor,texto):
+def clTxt(texto,cor):  # Adicionar caracteres coloridos
     return cores[cor]+texto+cores['padrao']
 
 def validaTentativa(chute):
-    if chute not in listaescolhida:  # Verifica se a entrada é uma palavra
-        print('Palavra não conhecida, tente outra')
+    if chute not in listaescolhida:  # Verifica se a entrada é uma palavra dentro da lista de palavras
+        return ('Palavra não conhecida, tente outra',False)
     else:
         comparar=inidica_posicao(dicio_inicio['sorteada'],chute)
         if comparar == []:
-            print(f'essa palavra não possui {letras} letras, tente novamente com uma palavra válida ')
-            tentativa=tentativa
+            return(f'essa palavra não possui {letras} letras, tente novamente com uma palavra válida ',False)
         else:
-            parcial=''
+            global tentativa
+            
+            parcial=[]
             for pos in range(len(comparar)):
                 if comparar[pos]==0:
-                    parcial+=(f'\033[0;32;40m {chute[pos]} \033[m')
+                    parcial.append(clTxt(chute[pos],'correta'))
                 elif comparar[pos]==1:
-                    parcial+=(f'\033[0;33;40m {chute[pos]} \033[m')
+                    parcial.append(clTxt(chute[pos],'parcial'))
                 elif comparar[pos]==2:
-                    parcial+=(f'\033[0;37;40m {chute[pos]} \033[m')
-            print(parcial)
+                    parcial.append(clTxt(chute[pos],'neutra'))
+
             a=comparar.count(0)
             if a==letras:
                 tentativa=dicio_inicio['tentativas']
             else:
                 tentativa+=1
-        if a==letras and tentativa<=dicio_inicio['tentativas']:
-            print('\033[1;30;43m PARABÉNS, VOCÊ DESCOBRIU A PALAVRA \033[m')
-        elif a!=letras and tentativa==dicio_inicio['tentativas']:
-            print(f'\033[1;31;40m que pena, você não descobriu... A resposta era {resposta} \033[m')
+            return (parcial,True)
     
     
 
@@ -92,5 +92,18 @@ resposta=dicio_inicio['sorteada']
 tentativa=0
 while tentativa<dicio_inicio['tentativas']:
     chute=input(f'Tente uma palavra com {letras} letras: ')
-    validaTentativa(chute)
+
+    (res,valid) = validaTentativa(chute)
+    if valid:
+        dicio_inicio['especuladas'].append(res)
+    else:
+        print(res)
+
     print(crTabela(dicio_inicio['especuladas'],letras,dicio_inicio['tentativas']))
+
+
+# Sequência fora do loop de jogo
+if a==letras and tentativa<=dicio_inicio['tentativas']:  # Caso o usuário tenha descoberto a palavra
+    print(f'{cores["correta"]} PARABÉNS, VOCÊ DESCOBRIU A PALAVRA {cores["neutra"]}')
+elif a!=letras and tentativa==dicio_inicio['tentativas']:  # Caso o usuário não tenha conseguido achar a palavra
+    print(f'{cores["errada"]} Que pena, você não descobriu... A resposta era {resposta}{cores["neutra"]}')
